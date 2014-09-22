5effcd8f4867c88768d0e13a288d9650ac3ea504
dantetam <dante.tam1@gmail.com>
2014-09-21 20:51:54 -0700

    Added enemies + bullets

--- main.lua
+++ main.lua
@@ -6,31 +6,56 @@
 
 --display.newText("Hello World",100,100,native.systemFont,15)
 
-local player = display.newRect(50,50,50,50)
+local player = display.newRect(50,50,20,20)
 local wP,aP,sP,dP = false
+
 local projectiles = {}
-local xSpeeds = {}
-local ySpeeds = {}
-local debounce = true
+local pSpeedsX = {}
+local pSpeedsY = {}
+
+local enemies = {}
+local eSpeedsX = {}
+local eSpeedsY = {}
+
+local lastBullet = 0
 local frames = 0
 
 function checkTime(event)
-	if frames % 10 == 0 then debounce = true end
+	--if frames % 10 == 0 then debounce = true end
+	--if system.getTimer() - lastBullet >= 1000 then player.fill = {0,255,0} else player.fill = {255,0,0} end
 	frames = frames + 1
 	local speed = 3
 	if wP then player:translate(0,-speed) end
 	if aP then player:translate(-speed,0) end
 	if sP then player:translate(0,speed) end
 	if dP then player:translate(speed,0) end
+	for i = 1,table.getn(enemies),1 do
+		if math.random() < 0.1 then
+			eSpeedsX[i] = math.random()*5 - 2.5
+			eSpeedsY[i] = math.random()*5 - 2.5
+		end
+		enemies[i]:translate(eSpeedsX[i],eSpeedsY[i])
+		if enemies[i].x < 0 then
+			enemies[i].x = 0
+		elseif enemies[i].y < 0 then
+			enemies[i].y = 0
+		elseif enemies[i].x > display.contentWidth then
+			enemies[i].x = display.contextWidth
+		elseif enemies[i].y > display.contentHeight then
+			enemies[i].y = display.contextHeightdssad
+		end
+	end
 	for i = 1,table.getn(projectiles),1 do
-		projectiles[i]:translate(xSpeeds[i],ySpeeds[i])
+		projectiles[i]:translate(pSpeedsX[i],pSpeedsY[i])
+		pSpeedsX[i] = pSpeedsX[i]*1.07
+		pSpeedsY[i] = pSpeedsY[i]*1.07
 	end
 	for i = table.getn(projectiles),1,-1 do
 		if projectiles[i].x < 0 or projectiles[i].y < 0 or projectiles[i].x > display.contentWidth or projectiles[i].y > display.contentHeight then
 			local p = projectiles[i]
 			table.remove(projectiles, i)
-			table.remove(xSpeeds, i)
-			table.remove(ySpeeds, i)
+			table.remove(pSpeedsX, i)
+			table.remove(pSpeedsY, i)
 			p:removeSelf()
 			i = i - 1
 		end
@@ -38,10 +63,13 @@ function checkTime(event)
 end
 
 function projectile(x,y,xSpeed,ySpeed)
-	local p = display.newCircle(x,y,5)
+	local p = display.newRect(x,y,2,10)
+	--print(math.atan2(ySpeed,xSpeed))
+	p:rotate(math.deg(math.atan2(ySpeed,xSpeed))+90)
 	table.insert(projectiles, p)
-	table.insert(xSpeeds, xSpeed)
-	table.insert(ySpeeds, ySpeed)
+	table.insert(pSpeedsX, xSpeed)
+	table.insert(pSpeedsY, ySpeed)
+	return p
 end
 
 function keyPressed(event)
@@ -61,17 +89,28 @@ function keyPressed(event)
 end
 
 function mousePressed(event)
-	if debounce then
-		debounce = false
-		if event.isPrimaryButtonDown then
+	if event.isPrimaryButtonDown then
+		if system.getTimer() - lastBullet >= 500 then
+			lastBullet = system.getTimer()
 			local deltaX = event.x - player.x
 			local deltaY = event.y - player.y
 			local angle = math.atan2(deltaY, deltaX)
-			projectile(player.x, player.y, math.cos(angle)*5, math.sin(angle)*5)
+			projectile(player.x, player.y, math.cos(angle)*10, math.sin(angle)*10)
 		end
 	end
 end
 
+function collide(a,b)
+	
+end
+
+for i=1,20,1 do
+	local enemy = display.newCircle(math.random()*display.contentWidth,math.random()*display.contentHeight,7.5)
+	table.insert(enemies, i, enemy)
+	table.insert(eSpeedsX,i,math.random()*5 - 2.5)
+	table.insert(eSpeedsY,i,math.random()*5 - 2.5)
+end
+
 Runtime:addEventListener("key", keyPressed)
 Runtime:addEventListener("mouse", mousePressed)
-timer.performWithDelay(1000/40, checkTime, 10000)
+timer.performWithDelay(1000/40, checkTime, 0)
